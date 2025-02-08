9c7ea9368443f469364d1a8deb91129d93312710
Steveq18 <steveql1886@gmail.com>
2025-02-07 23:45:57 -0600

    inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { IngresosComponent } from './Paginas/ingresos/ingresos.component';
+import { GastosComponent } from './Paginas/gastos/gastos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'Ingresos', component: IngresosComponent},
+  {path:'Gastos', component: GastosComponent}
+];
